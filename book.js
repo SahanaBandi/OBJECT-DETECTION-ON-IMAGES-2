707a11294d73f272f9de3d4d6fdfb0c6c40f4293
SahanaBandi <87894811+SahanaBandi@users.noreply.github.com>
2022-02-03 20:40:14 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,38 @@
+Status="";
+image="";
+function preload()
+{
+   img=loadImage("Book.jpg");
+}
+
+function setup()
+{
+    canvas=createCanvas(640,420);
+    canvas.position(215,300);
+    objectDetector=ml5.objectDetector('coco.ssd',modelLoaded);
+    document.getElementById("status").innerHTML="Status - Detecting Object";
+}
+
+function draw()
+{
+    image(img,0,0,640,420);
+}
+
+function modelLoaded()
+{
+    console.log("Model is loadede. Yayy!");
+    Status = true;
+    objectDetector.detect(img,gotresults);
+}
+
+function gotresults(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        console.log(results);
+    }
+}
